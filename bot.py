cefc5c2ad2d84fae76158138db6c938530f2c561
Cleone1 <43510177+Cleone1@users.noreply.github.com>
2018-09-23 01:25:52 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 import redis
 import os
 import telebot
@@ -12,9 +13,28 @@ some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
 # bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(TOKEN)
 # some_api = some_api_lib.connect(some_api_token)
+def select_response(message):
+	responses = ['Ie {} tio, no et canses?', '{}, eres un puto pesat de tio', 'Ie {}, ja hi ha prou que ja cansa',
+				'Collons {}, que pesat eres quan vols', "Que si {}, tio pesat, que ja t'hem llegit"]
+
+	response_to_use = random.choice(responses)
+	name = unicodedata.normalize('NFKD', message.from_user.first_name).encode('ascii','ignore')
+	response = response_to_use.format(name)
+
+	return response
+
+
+# The decorator (@bot.message_handler) indicates the type of messages that will activate this function
+# In this case, we'll activate it for every message. See telebot API for more possibilities 
+@bot.message_handler(func=lambda message: True)
+def pole_reply(message):
+	# If the message contains the word 'pole' (case insensitive), the bot replies
+	if 'pole' in message.text.lower():
+		resposta = select_response(message)
+		bot.reply_to(message, resposta)
 #              ...
